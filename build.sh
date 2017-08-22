a45e871c8803454e74dd7a461f140134cc88cdc2
sebagomez <sebagomezcorrea@outlook.com>
2017-08-22 10:44:17 -0300

    build files added

--- build.sh
+++ build.sh
@@ -1,2 +1,1 @@
-docker build -f java/tomcat/Dockerfile -t genexus/javatomcat java/tomcat
-#docker build -f .net core/Dockerfile -t genexus/javatomcat java/.net core
+./java/tomcat/build.sh
---
+++ java/tomcat/build.sh
@@ -0,0 +1,2 @@
+docker build -t genexus/javatomcat .
+docker push genexus/javatomcat
