04057a212f81e9d09b01d9088633fd1f260274f3
seba gomez <sebagomezcorrea@outlook.com>
2019-06-11 13:56:42 -0300

    improved image files and added a build script

---
+++ build-images.ps1
@@ -0,0 +1,30 @@
+$today = Get-Date
+$osType = docker info --format '{{.OSType}}'
+
+$javaImage = 'genexus/javatomcat:8.5'
+$netCoreImage = 'genexus/dotnetcore:2.1.1'
+$netImage = 'genexus/dotnet:4.6.2'
+
+if ($osType -eq 'linux'){
+	docker build --file .\java\tomcat\Dockerfile --build-arg BUILD_DATE=$today -t $javaImage .\java\tomcat\
+	docker push $javaImage
+}
+else{
+	Write-Warning "Java image will not be built. Switch Docker client to Linux containers."
+}
+if ($osType -eq 'linux'){
+	docker build --file '.\.net core\Dockerfile' --build-arg BUILD_DATE=$today -t $netCoreImage '.\.net core'
+	docker push $netCoreImage
+}
+else{
+	Write-Warning ".NET Core image will not be built. Switch Docker client to Linux containers."
+}
+if ($osType -eq 'windows'){
+	docker build --file .\.net\Dockerfile --build-arg BUILD_DATE=$today -t $netImage .\.net
+	docker push $netImage
+}
+else{
+	Write-Warning ".NET image will not be built. Switch Docker client to Windows containers."
+}
+
+
